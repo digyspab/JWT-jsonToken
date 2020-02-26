69e12713b2f0b7f8f2a8d79ea9b9f68ce91b5a86
Aditya <aditya.kumar@prolitus.com>
2020-02-26 14:57:32 +0530

    JSON data authentication and autherization

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
-var db = require('./db');
-
+var db = require('./db')();
+var port = process.env.PORT || 3000;
+// const InitiateMongoServer = require('./db')();
+var AuthController = require('./auth/AuthController');
 var UserController = require('./user/UserController');
+
+
 app.use('/users', UserController);
+app.use('/api/auth', AuthController);
+
+
+
+app.listen(port, function() {
+    console.log('Express server listening on port ' + port);
+  });
 
 module.exports = app;
--- db.js
+++ db.js
@@ -1,2 +1,17 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+const InitiateMongoServer = async () => {
+	try {
+		await mongoose.connect('mongodb://localhost:27017/JWT-jsonToken', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
+		console.log('Connected to MongoDB database...');
+	} catch(e) {
+		console.log(e);
+		throw e;
+	}
+
+	global.mongoDB = InitiateMongoServer;
+}
+
+module.exports = InitiateMongoServer;
